319f4bc0480b43b68509d5127dcc4a63ef1e25a8
kabul2022 <aziza.rezaie45@gmail.com>
2024-07-31 16:03:27 +0200

    added the axios integration so that  city weathershow

---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handelResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}℃`
+    );
+  }
+
+  let apiKey = "fed05438baea4bf5c4d86d8a94d310b9";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(handelResponse);
+
+  return <h1> Hello fromm Weather</h1>;
+}
